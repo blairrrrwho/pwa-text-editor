d4ffed32c729d1d92d343ab1121bec0b0e26ee0d
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-05-11 19:45:48 -0500

    changed code so that the app saves to local storage and works offline

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,8 +18,8 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  // const request = store.add({ text: content });
-  const request = store.put({ id: 1, value: content });
+  const request = store.add({ text: content });
+  // const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
 }
@@ -30,8 +30,8 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  // const request = store.get('text');
-  const request = store.get(1);
+  const request = store.get('text');
+  // const request = store.get(1);
   const result = await request;
   // console.log('result.value', result);
   // return result;
