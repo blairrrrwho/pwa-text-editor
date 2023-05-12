fd937926ef927057c1e7404f95083e7f07793912
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-05-11 21:15:08 -0500

    took out old code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,6 @@ export const putDb = async (content) => {
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.add({ text: content });
-  // const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
 }
@@ -31,10 +30,7 @@ export const getDb = async () => {
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get('text');
-  // const request = store.get(1);
   const result = await request;
-  // console.log('result.value', result);
-  // return result;
   result
     ? console.log("🚀 - data retrieved from the database", result.value)
     : console.log("🚀 - data not found in the database");
